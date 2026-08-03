b4e82ebe5932bf3900349b17781cc7334f5de8d0
DChiesa <121984753+DChiesa@users.noreply.github.com>
2026-08-03 14:45:05 -0300

    Add files via upload

--- portal-config.js
+++ portal-config.js
@@ -19,7 +19,8 @@ window.PORTAL_CONFIG=Object.freeze({
     'dashboard_rai_cluster.html':'atual/rai_cluster.xlsx',
     'backbone_analytics.html':'atual/eventos backbone.xlsx',
     'estudo_codigos_cluster.html':'atual/estudo codigo.xlsx',
-    'quebra_migracao.html':'atual/quebra_migracao.xlsx'
+    'quebra_migracao.html':'atual/quebra_migracao.xlsx',
+    'Quebra_Migracao.html':'atual/quebra_migracao.xlsx'
   })
 });
 window.APP_CONFIG=window.PORTAL_CONFIG;
